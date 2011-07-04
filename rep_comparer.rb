537e908e3cc34e0676274c3a4cc0e7e0c368b3e5
blackfoks <g.bragin@ftc.ru>
2011-07-04 15:40:21 +0700

    refactoring

--- rep_comparer.rb
+++ rep_comparer.rb
@@ -14,34 +14,40 @@ class ReportComparer
     old_rep = {}
     new_rep = {}
     
+    # read old file
     File.open(oldTxt.filename, 'r') do |old_file|
       old_file.lines.each do |line|
-        #old_rep << { :n => old_file.lineno, :text => line }
         old_rep[old_file.lineno] = line
       end      
     end
     
+    # read new file
     File.open(newTxt.filename, 'r') do |new_file|
       new_file.lines.each do |line|
         new_rep[new_file.lineno] = line
       end      
     end
     
+    # compare
     (0..new_rep.count).each do |i|
       if old_rep[i] && new_rep[i]
+        # if different
         if old_rep[i] != new_rep[i]
           diffs << { :old => old_rep[i], :new => new_rep[i] } 
         end
       else
+        # if no such line in old file
         if !old_rep[i]
           diffs << { :old => nil, :new => new_rep[i] }
         end
+        # if no such line in new line
         if !new_rep[i]
           diffs << { :old => old_rep[i], :new => nil }
         end
       end
     end
 
+    # show diff
     diffs.each do |diff|
       if diff[:old] || diff[:new]
         if /^.*\d+\.\d+\.\d+\s\d+:\d+.*$/ =~ diff[:old].encode('utf-8') || # date like 12.07.2011 13:45
