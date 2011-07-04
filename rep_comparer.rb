b6b53e225149ef2f7a3488d8631507b5640fe277
blackfoks <g.bragin@ftc.ru>
2011-07-04 15:08:57 +0700

    diffs now work

--- rep_comparer.rb
+++ rep_comparer.rb
@@ -1,3 +1,4 @@
+﻿# coding: utf-8
 
 class TxtReport
   attr_reader :filename
@@ -15,19 +16,42 @@ class ReportComparer
     
     File.open(oldTxt.filename, 'r') do |old_file|
       old_file.lines.each do |line|
-        old_rep << { :n => old_file.lineno, :text => line }
+        #old_rep << { :n => old_file.lineno, :text => line }
+        old_rep[old_file.lineno] = line
       end      
     end
     
-    File.open(oldTxt.filename, 'r') do |new_file|
+    File.open(newTxt.filename, 'r') do |new_file|
       new_file.lines.each do |line|
-        new_rep << { new_file.lineno => line }
+        new_rep[new_file.lineno] = line
       end      
     end
     
     (0..new_rep.count).each do |i|
-      puts new_rep[i]
-    end    
+      if old_rep[i] && new_rep[i]
+        if old_rep[i] != new_rep[i]
+          diffs << { :old => old_rep[i], :new => new_rep[i] } 
+        end
+      else
+        if !old_rep[i]
+          diffs << { :old => nil, :new => new_rep[i] }
+        end
+        if !new_rep[i]
+          diffs << { :old => old_rep[i], :new => nil }
+        end
+      end
+    end
+
+    diffs.each do |diff|
+      if diff[:old] || diff[:new]
+        puts "============== Различие ===============".encode('cp866')
+        puts "<<<======================"
+        puts diff[:old].encode('cp866', 'cp1251')
+        puts "========================="
+        puts diff[:new].encode('cp866', 'cp1251')
+        puts "======================>>>"
+      end
+    end
   end
 end
 
